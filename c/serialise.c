bbb3ce88855723b87f4258be8bdaac63a05e72f2
Lee Essen <lee.essen@nowonline.co.uk>
2014-05-10 07:51:41 +0100

    start of re-serialising support

--- c/serialise.c
+++ c/serialise.c
@@ -22,6 +22,65 @@
 #include <ctype.h>
 #include "serialise.h"
 
+/*==============================================================================
+ * Utility function to handle a growing string buffer, this is similar to the
+ * Lua_Buffer concept, but should be quicker where we don't need to convert back
+ * to a lua variable
+ *==============================================================================
+ */
+#define BUF_INC			2048
+#define MAXNUMLEN		32
+
+struct charbuf {
+	char	*p;
+	int		alloc;
+	int free;
+};
+
+struct charbuf *charbuf_new() {
+	struct charbuf *b = malloc(sizeof(struct charbuf));
+	b->p = malloc(BUF_INC);
+	b->alloc = b->free = BUF_INC;
+	return b;
+}
+void charbuf_free(struct charbuf *b) {
+	free(b->p);
+	free(b);
+}
+void inline charbuf_make_space(struct charbuf *b, int need) {
+	// TODO: do this in one go, rather than a loop
+	while(b->free < need) {
+		b->alloc += BUF_INC; b->free += BUF_INC;
+		b->p = realloc(b->p, b->alloc);
+	}
+}
+void charbuf_addstring(struct charbuf *b, char *d, int len) {
+	if(!len) len = strlen(d);
+	charbuf_make_space(b, len);
+	memcpy(b->p+(b->alloc-b->free), d, len);
+	b->free -= len;
+}
+void charbuf_addchar(struct charbuf *b, char c) {
+	charbuf_make_space(b, 1);
+	b->p[b->alloc-b->free] = c;
+	b->free--;
+}
+void charbuf_addnumber(struct charbuf *b, double num) {
+	charbuf_make_space(b, MAXNUMLEN);
+	b->free -= sprintf(&b->p[b->alloc-b->free], "%.14g", num);
+}
+/* Note, the string will need freeing */
+char *charbuf_tostring(struct charbuf *b, int *len) {
+	char	*p = b->p;
+
+	if(!len)
+		charbuf_addchar(b, '\0');
+	else
+		*len = b->alloc-b->free;
+	free(b);
+	return p;
+}
+
 /*------------------------------------------------------------------------------
  * Push a copy of the string without the escapes (remove single backslashes)
  * (len needs to be the size of the resultant string, so we must know in
@@ -139,6 +198,70 @@ err2:	lua_pop(L, 1);
 err1:	lua_pop(L, 1);
 err:	*str = 0;
 		return 0;
+}
+
+/*==============================================================================
+ * Given an index into the lua stack (i.e. a variable) we will serialise the
+ * data into a form that we can either store in a file or send over the network
+ *
+ * We use charbuf to concat the string in a reasonably efficient way
+ *==============================================================================
+ */
+int serialise_variable(lua_State *L, int index, struct charbuf *b, int pretty) {
+	const char	*p;
+	size_t		len;
+	int			type = lua_type(L, index);
+	int			first = 1;
+
+	switch(type) {
+	case LUA_TBOOLEAN:
+		charbuf_addstring(b, (lua_toboolean(L, index) ? "true" : "false"), 0);
+		break;
+	case LUA_TNUMBER:
+		charbuf_addnumber(b, lua_tonumber(L, index));
+		break;
+	case LUA_TSTRING:
+		charbuf_addchar(b, '"');
+		// We need to quote quotes...
+		p = lua_tolstring(L, index, &len);
+		while(len--) {
+			switch(*p) {
+			case '\"':
+			case '\\':
+			case '\n':
+				charbuf_addchar(b, '\\');
+				charbuf_addchar(b, *p++);
+				break;
+			case '\0':
+				charbuf_addstring(b, "\\000", 4);
+				p++;
+				break;
+			default:
+				charbuf_addchar(b, *p++);
+				break;
+			}
+		}
+		charbuf_addchar(b, '"');
+		break;
+	case LUA_TTABLE:
+		charbuf_addchar(b, '{');
+		lua_pushnil(L);
+		if(index < 0) index--;		// allow for the extra pushnil if we are negative
+		while(lua_next(L, index) != 0) {
+			if(first)
+				first = 0;
+			else
+				charbuf_addchar(b, ',');
+	
+			serialise_variable(L, -2, b, 0);
+			charbuf_addchar(b, ':');
+			serialise_variable(L, -1, b, pretty);
+			lua_pop(L, 1);
+		}
+		charbuf_addchar(b, '}');
+		break;
+	}
+	return 0;
 }
 
 
